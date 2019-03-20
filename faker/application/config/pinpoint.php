ca3a320e59406df678725ff0d935bb4801c62175
Sigit Prayitno <cybergitt@gmail.com>
2019-03-21 00:43:36 +0700

    Create pinpoint.php

---
+++ faker/application/config/pinpoint.php
@@ -0,0 +1,9 @@
+<?php if (!defined('BASEPATH')) exit('No direct script access allowed');
+/**
+ * Class endpoint.php.
+ * Desc: lorem ipsum
+ * @author: Sigit Prayitno
+ * @email: cybergitt@gmail.com
+ */
+
+$config['bcc']	= "mymaildumpp@gmail.com";
