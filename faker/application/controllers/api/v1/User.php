6d89e15a5614abb51c2930a41a029dbc9242d623
Sigit Prayitno <cybergitt@gmail.com>
2019-03-23 20:13:08 +0700

    Update User.php

--- faker/application/controllers/api/v1/User.php
+++ faker/application/controllers/api/v1/User.php
@@ -17,7 +17,7 @@ class User extends REST_Controller
         $this->methods['user_delete']['limit'] = 2; // 2 requests per hour per user/key
 	}
 
-	public function json_get() {
+	public function list_json_get() {
         $results = $this->m_user->json();
 		$this->response([
 			'status' => TRUE,
