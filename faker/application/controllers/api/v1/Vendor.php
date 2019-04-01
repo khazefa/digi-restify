375383a6f98596c3fa7a4530bc467b1e7de7403e
rahmat ramadhan <ramadhan.dont@gmail.com>
2019-04-01 14:28:06 +0700

    vendor
    oke

--- faker/application/controllers/api/v1/Vendor.php
+++ faker/application/controllers/api/v1/Vendor.php
@@ -5,8 +5,9 @@
 		function __construct(){
 			parent::__construct(); // Construct the parent class
 			$this->load->library('datatables'); //load library ignited-dataTable
-			$this->load->model('User_model','m_user'); // Call model to represent database table
+			$this->load->model('Vendor_model','m_vendor'); // Call model to represent database table
 			$this->load->helper('security');
+			$this->load->helper('string');
 
 			// Configure limits on our controller methods
 			// Ensure you have created the 'limits' table and enabled 'limits' within application/config/rest.php
@@ -14,68 +15,45 @@
 			$this->methods['register_post']['limit'] = 100; // 100 requests per hour per user/key
 			$this->methods['user_delete']['limit'] = 2; // 2 requests per hour per user/key
 		}
+		
+		
+		
 
 		public function list_json_get() {
 			$group = filter_var($this->input->get('groupid', TRUE), FILTER_SANITIZE_STRING);
 
-			$results = $this->m_user->json();
+			$results = $this->m_vendor->json();
 			$this->response([
 				'status' => TRUE,
 				'message' => 'data available '.$group,
 				'results' => $results
 			], REST_Controller::HTTP_OK);
 		}
+		
+		public function input_post() {
+			//$data = json_decode(file_get_contents("php://input"));
 			
-		/**
-		* This function is used to check email exist
-		*/
-			public function check_email_get()
-			{
-			$email = filter_var($this->input->get('email', TRUE), FILTER_SANITIZE_EMAIL);
+			$vendor_code 			= date('ymd'). random_string('nozero', 6);
+			$vendor_name 			= strip_tags($this->input->post('vendor_name', TRUE));
+			$vendor_owner 		= strip_tags($this->input->post('vendor_owner', TRUE));
+			$vendor_phone 		= strip_tags($this->input->post('vendor_phone', TRUE));
+			$vendor_mobile 		= strip_tags($this->input->post('vendor_mobile', TRUE));
+			$vendor_reg_user	= strip_tags($this->input->post('vendor_reg_user', TRUE));
 			
-			if($this->m_user->check_data_exists(array('user_email'=>$email)) > 0)
-			{
-				$this->response([
-					'status' => TRUE,
-					'message' => 'Account exists'
-				], REST_Controller::HTTP_OK);
-			}else{
-				$this->response([
-					'status' => FALSE,
-					'message' => 'Account not exists'
-				], REST_Controller::HTTP_BAD_REQUEST);
-			}
-		}
-
-		/**
-		 * Function user registration
-		 */
-		public function register_post()
-			{
-					$ukey = strip_tags($this->input->post('username', TRUE));
-					$password = $this->input->post('password', TRUE);
-					$name = strip_tags($this->input->post('name', TRUE));
-			$email = filter_var($this->input->post('email', TRUE), FILTER_SANITIZE_EMAIL);
-
-			// Password length should be at least 8 characters
-			if(strlen($password) < 8){
-				$this->response([
-					'status' => FALSE,
-					'message' => 'Your password should be at least 8 characters'
-				], REST_Controller::HTTP_BAD_REQUEST);
-			}else{
-				$data_post = array('user_key'=>$ukey, 'user_pass'=>getHashedPassword($password), 'user_name'=>$name, 'user_email'=>$email, 'created_at'=>date('Y-m-d H:i:s'));
+			if(!empty($vendor_name) && !empty($vendor_owner) && !empty($vendor_mobile)){
+			 
+				$data_post = array('vendor_code'=>$vendor_code, 'vendor_name'=>$vendor_name, 'vendor_owner'=>$vendor_owner, 'vendor_phone'=>$vendor_phone, 'vendor_mobile'=>$vendor_mobile, 'vendor_reg_user'=>$vendor_reg_user, 'vendor_reg_date'=>date('Y-m-d H:i:s'), 'vendor_status'=>1, 'vendor_drop'=>0, 'created_at'=>date('Y-m-d H:i:s') );
 				$data_post = $this->security->xss_clean($data_post);
 
-				$count = $this->m_user->check_data_exists(array('user_key' => $ukey));
-				if ($count > 0)
-				{
+				$count = $this->m_vendor->check_data_exists(array('vendor_code' => $vendor_code));
+				if ($count > 0){
 					$this->response([
 						'status' => FALSE,
-						'message' => 'username already exists'
+						'message' => 'vendorcode already exists'
 					], REST_Controller::HTTP_BAD_REQUEST);
-				}else{
-					$result = $this->m_user->insert_data($data_post);
+				}
+				else{
+					$result = $this->m_vendor->insert_data($data_post);
 				
 					if($result[0])
 					{
@@ -93,76 +71,76 @@
 						], REST_Controller::HTTP_BAD_REQUEST);
 					}
 				}
+			
 			}
-		}
-
-		/**
-		 * Function user update
-		 */
-		public function update_post()
-			{
-					$ukey = strip_tags($this->input->post('username', TRUE));
-					$password = $this->input->post('password', TRUE);
-					$name = strip_tags($this->input->post('name', TRUE));
-			$email = filter_var($this->input->post('email', TRUE), FILTER_SANITIZE_EMAIL);
-
-			if(empty($password)){
-				$data_post = array('user_name'=>$name, 'updated_at'=>date('Y-m-d H:i:s'));
-			}else{
-				// Password length should be at least 8 characters
-				$data_post = array('user_pass'=>getHashedPassword($password), 'user_name'=>$name, 'updated_at'=>date('Y-m-d H:i:s'));
-			}
-
-			$update_email = $this->m_user->check_data_exists(array('user_email' => $email)) >= 1 ? FALSE : TRUE;
-
-			if($update_email){
-				$data_post += ['user_email'=>$email];
+			else{
+				$this->response([
+					'status' => FALSE,
+					'message' => 'Ada data kosong'
+				], REST_Controller::HTTP_BAD_REQUEST);
 			}
 			
-			$data_post = $this->security->xss_clean($data_post);
-
-			$result = $this->m_user->update_data($data_post, $ukey);
+		}
+	
+		public function update_post(){
 		
-			if($result[0])
-			{
+			$vendor_code 			= strip_tags($this->input->post('vendor_code', TRUE));
+			$vendor_name 			= strip_tags($this->input->post('vendor_name', TRUE));
+			$vendor_owner 		= strip_tags($this->input->post('vendor_owner', TRUE));
+			$vendor_phone 		= strip_tags($this->input->post('vendor_phone', TRUE));
+			$vendor_mobile 		= strip_tags($this->input->post('vendor_mobile', TRUE));
+			$vendor_status		= strip_tags($this->input->post('vendor_status', TRUE));
+			$vendor_drop			= strip_tags($this->input->post('vendor_drop', TRUE));
+
+			if(!empty($vendor_name) && !empty($vendor_owner) && !empty($vendor_mobile)){
+				$data_post = array('vendor_name'=>$vendor_name, 'vendor_owner'=>$vendor_owner, 'vendor_phone'=>$vendor_phone, 'vendor_mobile'=>$vendor_mobile, 'vendor_status'=>$vendor_status, 'vendor_drop'=>$vendor_drop, 'updated_at'=>date('Y-m-d H:i:s') );
+				
+				$data_post = $this->security->xss_clean($data_post);
+
+				$result = $this->m_vendor->update_data($data_post, $vendor_code);
+			
+				if($result[0])
+				{
+					$this->response([
+						'status' => TRUE,
+						'result' => $result[1],
+						'message' => 'success'
+					], REST_Controller::HTTP_OK);
+				}
+				else
+				{
+					$this->response([
+						'status' => FALSE,
+						'message' => 'failure some error occured:'.$result[1]
+					], REST_Controller::HTTP_BAD_REQUEST);
+				}
+			}
+			else{
+				$this->response([
+					'status' => FALSE,
+					'message' => 'Ada data kosong'
+				], REST_Controller::HTTP_BAD_REQUEST);
+			}
+		}
+		
+		
+		public function delete_post(){
+			$vendor_code = strip_tags($this->input->post('vendor_code', TRUE));
+
+			$result = $this->m_vendor->delete_data($vendor_code);
+
+			if($result[0]){
 				$this->response([
 					'status' => TRUE,
-					'result' => $result[1],
 					'message' => 'success'
 				], REST_Controller::HTTP_OK);
 			}
-			else
-			{
+			else{
 				$this->response([
 					'status' => FALSE,
 					'message' => 'failure some error occured:'.$result[1]
 				], REST_Controller::HTTP_BAD_REQUEST);
 			}
 		}
-		
-		/**
-			 * This function is used to delete the data using id
-			 * @return boolean $result : TRUE / FALSE
-			 */
-			function delete_post()
-			{
-					$ukey = strip_tags($this->input->post('username', TRUE));
 
-					$result = $this->m_user->delete_data($ukey);
-
-					if($result[0])
-					{
-							$this->response([
-									'status' => TRUE,
-									'message' => 'success'
-							], REST_Controller::HTTP_OK);
-					}
-					else
-					{
-				$this->response([
-					'status' => FALSE,
-					'message' => 'failure some error occured:'.$result[1]
-				], REST_Controller::HTTP_BAD_REQUEST);
-					}
-			}
 	}
